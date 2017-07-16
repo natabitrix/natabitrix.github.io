1104167f261b9624346eff8e37d5c954f17e0513
natabitrix <natabitrix@ya.ru>
2017-07-16 16:23:13 +0300

    Add files via upload

---
+++ js/groups.js
@@ -0,0 +1,73 @@
+// Группы объектов
+var groups = 
+[{
+    "id": "rf",
+    "name": "Россия",
+    "style": "islands#redIcon",
+    "items": [{
+        "id": "1",
+        "name": "Амурская область",
+        "style": "islands#redIcon",
+        "items": [{
+            "id": "2",
+            "center": [50.282329, 127.530997],
+            "name": "Благовещенск",
+            "style": ""
+        }, {
+            "id": "3",
+            "center": [55.152790, 124.741716],
+            "name": "Тында",
+            "style": ""
+        }]
+    }, {
+        "id": "4",
+        "name": "Воронежская область",
+        "style": "islands#greenIcon",
+        "items": [{
+            "id": "5",
+            "name": "Воронеж",
+            "style": "",
+            "center": [51.656441, 39.170241],
+        }, {
+            "id": "6",
+            "name": "Россошь",
+            "style": "",
+            "center": [50.200398, 39.550472],
+        }]
+    }]
+}, {
+    "id": "sng",
+    "name": "СНГ",
+    "style": "islands#blueIcon",
+    "items": [{
+        "id": "7",
+        "name": "Армения",
+        "style": "islands#blueIcon",
+        "items": [{
+            "id": "8",
+            "name": "Ереван",
+            "style": "",
+            "center": [40.161241, 44.521868],
+        }, {
+            "id": "9",
+            "name": "Джрвеж",
+            "style": "",
+            "center": [40.188971, 44.622324],
+        }]
+    }, {
+        "id": "10",
+        "name": "Казахстан",
+        "style": "islands#orangeIcon",
+        "items": [{
+            "id": "11",
+            "name": "Алматы",
+            "style": "",
+            "center": [43.228146, 76.933859],
+        }, {
+            "id": "12",
+            "name": "Астана",
+            "style": "",
+            "center": [51.139148, 71.364053],
+        }]
+    }]
+}]
